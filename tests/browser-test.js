ad66a3bb67568aed00bfdd42200b85daf78beddf
axel669 <haryuu.hanekata@gmail.com>
2018-01-07 19:44:55 -0800

    adding tests

---
+++ tests/browser-test.js
@@ -0,0 +1,12 @@
+var now = new Chrono();
+
+console.log(now.format("[testing] hh:mm:ss"));
+
+// const now = Chrono("Mon Jan 01 2018 20:17:09 GMT-0800");
+// const now2 = Chrono("Mon Jan 01 2018 20:17:09 GMT-0700");
+// const other = now.shift(1, 'hour');
+//
+// console.log(now);
+// console.log(other);
+// console.log(now.dif(other));
+// console.log(now.time, now2.time, now.time - now2.time);
---
+++ tests/tests.js
@@ -0,0 +1,6 @@
+const Chrono = require('../chrono');
+//  lets me use the browser tests in node as well
+//  to make sure it works in both envs
+//  I DO NOT recommend doing this in production code
+global.Chrono = Chrono;
+require("./browser-test");
